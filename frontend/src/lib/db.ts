3dd3d7f7d7f1fed51794f1a954f07930903a9e96
srija2228 <srija.reddy1028@gmail.com>
2026-05-25 03:15:16 +0530

    fixes

--- frontend/src/lib/db.ts
+++ frontend/src/lib/db.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DIRECT_URL ?? process.env.DATABASE_URL,
-      },
-    },
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
